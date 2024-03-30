bffda5835b43e9218edec4605f7a9a58b8191024
Koen <hicswk7278@clarkstate.edu>
2024-03-29 20:17:46 -0400

    Clarify mathmatics

--- src/main/java/frc/robot/mathHelpers/Trigonometry.java
+++ src/main/java/frc/robot/mathHelpers/Trigonometry.java
@@ -6,6 +6,7 @@ public class Trigonometry {
     static public double[] cartesianToSpherical(double x, double y, double z) {
         // Multiply variables by themselves and not use Math.pow for efficiency
         double range = Math.sqrt(x * x + y * y + z * z);
+        // This is the yaw in spherical coordinates. Use the arc tangent of y and x to get the angle from the center of the april tag to the robot
         double azimuth = Math.atan2(y, x);
         double elevation = Math.atan2(z, Math.sqrt(x * x + y * y));
         return new double[] { azimuth, elevation, range };
